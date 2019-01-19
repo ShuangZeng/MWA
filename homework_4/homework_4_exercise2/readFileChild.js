8c568c205e1af339742f86faf49927058a00cd6a
ShuangZeng <44349698+ShuangZeng@users.noreply.github.com>
2019-01-18 22:49:21 -0600

    Update readFileChild.js

--- homework_4/homework_4_exercise2/readFileChild.js
+++ homework_4/homework_4_exercise2/readFileChild.js
@@ -1,20 +1,14 @@
 const fs = require('fs')
 const path = require('path')
-
-
 var readfile = function(pathName){
     const readStream = fs.createReadStream(pathName);
     readStream.on("data", (data) => {
-        // console.log('chunck: '+data)
         process.send(data.toString());
-    })
-    
+    })    
     readStream.on("end", (end) => {
-        // console.log('end')
         process.send("asaad");
     })
 }
-
 process.on('message', file =>{
     var filename = path.join(__dirname, file);
     readfile(filename);
