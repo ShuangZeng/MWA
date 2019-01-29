a361e612b2b9db6e872d98ed9799b716b8544295
ZengShuang <zshuang@mum.edu>
2019-01-29 00:55:09 -0600

    angular_day1

---
+++ homework_10/my-lab10-app/src/app/counter-component.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, Output , EventEmitter} from '@angular/core';
+
+
+@Component({
+  selector: 'app-counter-component',
+  template: `
+    <h1><p class = "red">{{message}} </p></h1>
+    <button (click)="increase()"> + </button>
+    {{value}}
+    <button (click) = "decrease()"> - </button>
+    <p><ng-content></ng-content></p>
+  `,
+  styles: ['p.red { color: red; }'],
+})
+
+export class CounterComponent{
+  @Input() message;
+  @Input() value;
+  @Input() componentCounterValue;
+  @Output() counterChange = new EventEmitter();
+  increase(){
+    this.value = this.value + 1;
+    this.counterChange.emit(this.value);
+    //return false;
+  }
+  decrease(){
+    this.value = this.value - 1;
+    this.counterChange.emit(this.value);
+    //return false;
+  }
+}
